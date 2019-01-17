d2174eb5024c31d05ee35841d0d43726580ccf22
Erez Makavy <erezm@wix.com>
2019-01-17 15:57:40 +0200

    Add some wsr components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Checkbox from 'wix-style-react/Checkbox';
+import Card from 'wix-style-react/Card';
+import Input from 'wix-style-react/Input';
+import FormField from 'wix-style-react/FormField';
+import Slider from 'wix-style-react/Slider';
 
 class App extends Component {
   render() {
@@ -20,7 +23,34 @@ class App extends Component {
           >
             Learn React
           </a>
-          <Checkbox>Helloe Wix-Style-React</Checkbox>
+          <div
+            data-hook="card-example-basic"
+            style={{background: '#F0F4F7', padding: 30}}
+          >
+            <Card>
+              <Card.Header title="Simple Card" />
+
+              <Card.Content>
+                <FormField label="Type your joke below">
+                  <Input />
+                </FormField>
+              </Card.Content>
+
+              <Card.Divider />
+
+              <Card.Content>
+                <FormField label="Type your tragedy below">
+                  <Input />
+                </FormField>
+                <Slider
+                  dataHook={'story-slider-multiple'}
+                  value={[3, 4, 5]}
+                  min={1}
+                  max={10}
+                />
+              </Card.Content>
+            </Card>
+          </div>
         </header>
       </div>
     );
